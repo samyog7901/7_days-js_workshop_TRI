dbcbb96240e86d14edf933a29d6e2e788fe578d1
samyog7901 <samyogkhadka247@gmail.com>
2024-10-24 19:32:50 +0545

    day4js

---
+++ day4.js
@@ -0,0 +1,49 @@
+//conditional statements
+// var isRaining = true
+// if(isRaining){
+//     console.log("Carry umbrella")
+// }
+// else{
+//     console.log("Don't carry umbrella")
+// }
+
+// //ternary operator 
+// isRaining ? console.log("Carry umbrella") :  console.log("Don't carry umbrella")
+
+// short-circuit
+// var isRaining = false
+// var isRaining = true
+// isRaining && console.log("carry umbrela")
+
+
+//if-else-if
+// var tempr = 31
+// if(tempr > 30){
+//     console.log("its hot outside")
+// }
+// else if(tempr > 20){
+//     console.log("its moderate outside")
+// }
+// else{
+//     console.log("its cold outside")
+// }
+
+//loop in js
+/*
+for
+for of 
+for in
+*/
+
+var days = ['Sunday', 'Monday', 'Tuesday','Wednesday','Thursday']
+//for loop-->for everything
+// for(var i = 0;i < days.length;i++){
+//     console.log(days[i])
+// }
+
+//for of loop(specialized for only arrays)
+for(let day of days){
+    console.log(day)
+}
+
+//for in-->loop for object only
